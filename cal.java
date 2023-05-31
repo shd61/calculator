175509f0a4e241f163633e131522d0b38c4922f4
Himasha Dissanayake <75574573+shd61@users.noreply.github.com>
2023-05-31 17:23:46 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class cal{
+	public static void main(String args []){
+	Scanner sun = new Scanner(System.in);
+	double no1,no2,ans;
+	System.out.println("Enter First Number : ");
+	no1 = sun.nextDouble();
+	System.out.println("Enter First Number : ");
+	no2 = sun.nextDouble();
+	ans = no1 + no2;
+	System.out.println("Answer is : " + ans);
+	
+	}
+}
